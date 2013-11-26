2a5d08e4b9fcc4d88547df52b12191afd530ecca
Refael Ackermann <refael@empeeric.com>
2013-11-26 11:18:30 +0200

    Simplified harness + compatible with travis + compatible with windows

--- test/promises.Aplus.js
+++ test/promises.Aplus.js
@@ -1,9 +1,7 @@
-
 /**
  * Module dependencies.
  */
 
-var assert = require('assert')
 var Promise = require('../lib/promise');
 var aplus = require('promises-aplus-tests');
 
@@ -23,19 +21,19 @@ adapter.rejected = function (reason) {
 adapter.deferred = function () {
   var p = new Promise;
   return {
-      promise: p
-    , reject: p.reject.bind(p)
-    , resolve: p.fulfill.bind(p)
+    promise: p, reject: p.reject.bind(p), resolve: p.fulfill.bind(p)
   }
 }
 
-aplus(adapter, {
-  reporter: 'spec'
-  , slow: 1
+it("run A+ suite", function (done) {
+  this.timeout(60000);
+  aplus(adapter, {
+    reporter: 'spec', slow: 1
 
 //    , bail:true
 //    , grep:'2.3.1: If `promise` and `x` refer to the same object, reject `promise` with a `TypeError` as the reason. via return from a fulfilled promise'
   }, function (err) {
-  assert.ifError(err);
+    done(err);
+  });
 });
 
