449331bde39cf5fc62ddf1629c40c1e5c67a859d
Refael Ackermann <refael@empeeric.com>
2014-01-26 01:08:09 +0200

    taking a page from the A+ testbook

--- test/promises.Aplus.js
+++ test/promises.Aplus.js
@@ -4,30 +4,12 @@
 var Promise = require('../lib/promise');
 var aplus = require('promises-aplus-tests');
 
-
-// Adapter
-var adapter = {
-  fulfilled: function (value) {
-    var p = new Promise;
-    p.fulfill(value);
-    return p;
-  },
-  rejected: function (reason) {
-    var p = new Promise;
-    p.reject(reason);
-    return p;
-  },
-  deferred: function () {
-    var d = Promise.deferred();
-    d.resolve = d.fulfill;
-    return d;
-  }
-};
-
-
 // tests
 describe.only("run A+ suite", function () {
-  this.timeout(3000);
-  aplus.mocha(adapter);
+  aplus.mocha({
+    fulfilled: Promise.fulfilled,
+    rejected: Promise.rejected,
+    deferred: Promise.deferred
+  });
 });
 
